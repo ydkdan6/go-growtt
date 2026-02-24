85e378e421a88a7685d9fa73b5adfb3e66783d4a
ydkdan6 <ydkdan6@gmail.com>
2026-02-24 23:34:25 +0100

    Fix import path for Toaster component in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import { Switch, Route, useLocation } from "wouter";
 import { BrowserRouter } from "react-router-dom";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
+import { Toaster } from "./components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { ThemeProvider } from "@/components/theme-provider";
 import { SidebarProvider } from "@/components/ui/sidebar";
