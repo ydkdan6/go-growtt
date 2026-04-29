add829691bf496027d3b0ac82f13d2eeaee73cda
Odiaga Raymond <odiagaraymondray@gmail.com>
2026-04-29 10:25:17 +0100

    chore: force vercel redeploy

--- server/index.ts
+++ server/index.ts
@@ -93,8 +93,7 @@ app.use((req, res, next) => {
   httpServer.listen(
     {
       port,
-      host: "0.0.0.0",
-      reusePort: true,
+      host: "127.0.0.1",
     },
     () => {
       log(`serving on port ${port}`);
